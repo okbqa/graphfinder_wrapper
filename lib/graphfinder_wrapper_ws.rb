12e4cd64fd02f58f63c45146de2552671e3e6c00
Jin-Dong Kim <jindong.kim@gmail.com>
2015-01-09 20:29:23 +0900

    url for graphfinder fixed.

--- lib/graphfinder_wrapper_ws.rb
+++ lib/graphfinder_wrapper_ws.rb
@@ -13,8 +13,8 @@ class GraphFinderWrapperWS < Sinatra::Base
 	end
 
 	before do
-		# graphfinder_url = "http://ws.lodqa.org:38502/queries"
-		graphfinder_url = "http://localhost:9292/queries"
+		graphfinder_url = "http://ws.lodqa.org:38502/queries"
+		# graphfinder_url = "http://localhost:9292/queries"
     @graphfinder_ws = RestClient::Resource.new graphfinder_url, :headers => {:content_type => :json, :accept => :json}
 
 		@params = JSON.parse request.body.read, :symbolize_names => true if request.body && request.content_type && request.content_type.downcase == 'application/json'
